a3be7ff8aa7c43eb522602a848eae5ce513c0c79
Michael Li <mic_lee2000@hotmail.com>
2023-10-09 17:12:28 +0800

    Experiment-1

--- src/nettools/qperf.go
+++ src/nettools/qperf.go
@@ -94,6 +94,7 @@ func cleanQPerfServer() {
 		cmdtext := line[len(line)-5:]
 		if cmdtext == "qperf" {
 			pid := strings.Split(line, " ")[0]
+			fmt.Println("qperf pid: " + pid)
 			if len(pid) > 0 {
 				killQperf(pid)
 			}
